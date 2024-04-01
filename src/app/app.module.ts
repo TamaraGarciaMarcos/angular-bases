5578106e6583f8044305b8056f887b3fc59330de
Tamara García <tamaragarciamarcos@gmail.com>
2024-04-01 15:48:24 +0200

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HeroesModule } from './heroes/heroes.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { DbzModule } from './dbz/dbz.module';
 
 
 @NgModule({
@@ -17,7 +17,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
